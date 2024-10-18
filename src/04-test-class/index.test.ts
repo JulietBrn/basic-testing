cfda00638e256135ad410e83a92e353120bb221a
JulietBrn <shishkina.ja@gmail.com>
2024-10-18 17:40:24 +0200

    feat: partly implement tasks 4

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,33 +1,49 @@
 // Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import { BankAccount, getBankAccount, InsufficientFundsError } from '.';
 
 describe('BankAccount', () => {
+  let account: BankAccount;
+
+  beforeEach(() => {
+    account = getBankAccount(100);
+  })
+
   test('should create account with initial balance', () => {
-    // Write your test here
+    account = getBankAccount(200);
+    expect(account.getBalance()).toBe(200);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    expect(() => account.withdraw(1000)).toThrow(InsufficientFundsError);
   });
 
   test('should throw error when transferring more than balance', () => {
-    // Write your test here
+    expect(() => account.withdraw(1000)).toThrow('Insufficient funds: cannot withdraw more than ')
+
   });
 
   test('should throw error when transferring to the same account', () => {
-    // Write your test here
+    expect(() => account.transfer(10, account)).toThrow('Transfer failed')
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    const result = account.deposit(50).getBalance();
+    expect(result).toBe(150);
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    const result = account.withdraw(30).getBalance();
+    expect(result).toBe(70);
   });
 
   test('should transfer money', () => {
-    // Write your test here
+    const accout2 = getBankAccount(0);
+    account.transfer(10, accout2)
+
+    const accountRes = account.getBalance()
+    const account2Res = accout2.getBalance()
+    expect(accountRes).toBe(90);
+    expect(account2Res).toBe(10);
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
